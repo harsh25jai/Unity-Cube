fe856c4e3e4c9c2d1b2b1c2e741b8ecdd1367037
H <>
2025-10-11 14:54:07 +0530

    Changes in Script for tutorial mode and some improvements related to folder structuring

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -23,8 +23,6 @@ public class BlockSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PlayerPrefs.DeleteAll();
-
         highScore = PlayerPrefs.GetInt("HighScore");
 
         // Check first run once
